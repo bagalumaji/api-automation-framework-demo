72ef6c9250505993897d687f7496413d41b745e8
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-19 21:02:06 +0530

    api end point constants

---
+++ src/main/java/com/coforge/endpoints/EndPoints.java
@@ -0,0 +1,10 @@
+package com.coforge.endpoints;
+
+import lombok.Getter;
+
+
+public class EndPoints {
+    private @Getter static final String loginEndPoint = "/login";
+    private @Getter static final String logoutEndPoint = "/logout";
+    private @Getter static final String accountEndPoint = "/account";
+}
