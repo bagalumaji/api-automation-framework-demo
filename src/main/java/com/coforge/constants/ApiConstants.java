2227417ad8cdb3da9b037f137d45e0037e523daf
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-19 21:01:22 +0530

    api constants

---
+++ src/main/java/com/coforge/constants/ApiConstants.java
@@ -0,0 +1,13 @@
+package com.coforge.constants;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class ApiConstants {
+    private @Getter static final String baseUri ="https://demo.testfire.net/api";
+    private @Getter @Setter
+    static String authorizationToken;
+
+    private @Getter static final String contentType= "application/json";
+
+}
