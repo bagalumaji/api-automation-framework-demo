634f59de09295562d1249e9be9137bc4d37ceaea
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-19 21:03:40 +0530

    framework listeners : ITest Listener and ISuiteListener

---
+++ src/main/java/com/coforge/listeners/FrameworkListener.java
@@ -0,0 +1,40 @@
+package com.coforge.listeners;
+
+import com.coforge.report.ExtentReport;
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.annotations.Test;
+
+public class FrameworkListener implements ITestListener, ISuiteListener {
+    @Override
+    public void onStart(ISuite suite) {
+        ExtentReport.initReport();
+    }
+
+    @Override
+    public void onFinish(ISuite suite) {
+        ExtentReport.flushReport();
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentReport.createTest(result.getMethod().getConstructorOrMethod().getMethod().getAnnotation(Test.class).description());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        ITestListener.super.onTestSuccess(result);
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        ITestListener.super.onTestFailure(result);
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        ITestListener.super.onTestSkipped(result);
+    }
+}
