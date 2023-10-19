4c9f3cf16784083564ca4f5b46c5305045afb712
Umaji.Bagal <bagalumaji@gmail.com>
2023-10-19 21:01:51 +0530

    demo test

---
+++ src/test/java/com/coforge/tests/rough/DemoTest.java
@@ -0,0 +1,11 @@
+package com.coforge.tests.rough;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+    @Test
+    public void demoTest(){
+       String baseProjectPath =  System.getProperty("user.dir");
+        System.out.println("baseProjectPath = " + baseProjectPath);
+    }
+}
